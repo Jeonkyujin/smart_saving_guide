932c2156e7d4307d620b551a28da44c1fe614767
jeonkyujin <954qew@naver.com>
2025-05-31 20:53:27 +0900

    chore: initial commit

---
+++ src/main/java/smart_saving_guide/example/smart_saving_guide/SmartSavingGuideApplication.java
@@ -0,0 +1,13 @@
+package smart_saving_guide.example.smart_saving_guide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartSavingGuideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartSavingGuideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/smart_saving_guide/example/smart_saving_guide/SmartSavingGuideApplicationTests.java
@@ -0,0 +1,13 @@
+package smart_saving_guide.example.smart_saving_guide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartSavingGuideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
